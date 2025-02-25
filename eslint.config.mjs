673509e4b3df4a6dc468446369d3ed81ed552f5a
yuya.shiraishi <yuya.shiraishi@arsaga.jp>
2025-02-25 09:25:09 +0900

    Markup memo list screen

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,18 +2,22 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+import configPrettier from "eslint-config-prettier";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.node }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  configPrettier, // Prettier 設定を追加
   {
+    plugins: { prettier: pluginPrettier },
     rules: {
-      "react/react-in-jsx-scope": 0,
+      "prettier/prettier": "error", // Prettier ルールを ESLint でチェック
+      "react/react-in-jsx-scope": "off",
     }
   }
-]
+];
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,62 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const Index = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月1日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        <View>
+
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月1日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月1日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <Text>+</Text>
+      </View>
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+})
+
 export default Index
