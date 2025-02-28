8c2f77796315ea8ec1ce27a52b8359149539a5b7
yuya.shiraishi <yuya.shiraishi@arsaga.jp>
2025-02-28 17:31:57 +0900

    ✨ボタンのスタイリング

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,6 @@ import { View, Text, StyleSheet } from "react-native";
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-
       <View style={styles.header}>
         <View style={styles.headerInner}>
           <Text style={styles.headerTitle}>Memo App</Text>
@@ -12,7 +11,6 @@ const Index = (): JSX.Element => {
       </View>
 
       <View>
-
         <View style={styles.memoListItem}>
           <View>
             <Text style={styles.memoListItemTitle}>買い物リスト</Text>
@@ -42,26 +40,24 @@ const Index = (): JSX.Element => {
             <Text>X</Text>
           </View>
         </View>
-
       </View>
 
-      <View>
-        <Text>+</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
       </View>
-
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: "#fff",
   },
   header: {
     backgroundColor: "#467FD3",
     height: 104,
-    justifyContent: "flex-end"
+    justifyContent: "flex-end",
   },
   headerInner: {
     alignItems: "center",
@@ -96,8 +92,29 @@ const styles = StyleSheet.create({
   memoListItemDate: {
     fontSize: 12,
     lineHeight: 16,
-    color: "#848484"
-  }
-})
+    color: "#848484",
+  },
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: "#467FD3",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 40,
+    right: 40,
+    shadowColor: "#000",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 8 },
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: "#fff",
+    fontSize: 40,
+    lineHeight: 40,
+  },
+});
 
-export default Index
+export default Index;
